9c8976f9dd2ef888defd347bfb5f6a6528797673
vikas <vkassharma19492@gmail.com>
2023-03-02 02:52:22 +0530

    phonebook

---
+++ part1b/src/App.js
@@ -0,0 +1,14 @@
+import useState from 'react';
+
+const App = () => {
+
+  const [persons, setPersons] = useState([])
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+    </div>
+  )
+}
+
+export default App;
