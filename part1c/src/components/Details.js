87264347fd43e161852b89452df753f215c131a5
vikas <vkassharma19492@gmail.com>
2023-03-16 19:06:53 +0530

    countries

--- part1c/src/components/Details.js
+++ part1c/src/components/Details.js
@@ -1,17 +1,25 @@
 const Details = ({ countries }) => {
     return (
         <div>
-            {countries.map(
-                country => <ul key={country.name}>
-                    <li>name:{country.name}</li>
-                    <li>capital:{country.capital}</li>
-                    <li>area:{country.area}</li>
+            {countries.map(country => (
+                <ul key={country.name}>
+                    <li>name: {country.name}</li>
+                    <li>capital: {country.capital}</li>
+                    <li>area: {country.area}</li>
+                    <li>
+                        languages:
+                        {country.language
+                            ? Object.values(country.language).map(lang => (
+                                <option key={lang}>{lang}</option>
+                            ))
+                            : "N/A"}
+                    </li>
+
                     <li>flag: {country.flag}</li>
                 </ul>
-            )}
+            ))}
         </div>
-    )
-}
-
+    );
+};
 
 export default Details;
