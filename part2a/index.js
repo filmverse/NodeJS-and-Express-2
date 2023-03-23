421c63b9c71bbb88fdedb9b67d4fc750d2683601
vikas <vkassharma19492@gmail.com>
2023-03-24 02:10:44 +0530

    handling post data

--- part2a/index.js
+++ part2a/index.js
@@ -48,7 +48,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 const generateId = () => {
     const maxId = notes.length > 0
-        ? Math.max(...notes.map(note => note.id))
+        ? Math.max(...notes.map(n => n.id))
         : 0
     return maxId + 1
 }
