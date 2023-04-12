d99151fba88fc3500881d1f633a209498cefc4c4
vkas sharma <vkassharma19492@gmail.com>
2023-04-12 18:43:47 +0530

    connecting backend to frontend, first step, added cors middleware in backed

--- part3a/index.js
+++ part3a/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,6 +9,7 @@ const unknownEndpoint = (request, response) => {
     })
 }
 
+app.use(cors())
 app.use(express.json())
 
 let notes = [
