f2d55040559956d5baf5cdfbd600dab72f9a2727
vkas sharma <vkassharma19492@gmail.com>
2023-04-16 02:36:01 +0530

    adding MongoDB

--- part3a/mongo.js
+++ part3a/mongo.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
     console.log('give password as argument')
     process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://vkassharma19492:<password>@cluster0.paffoup.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://vkassharma19492:${password}@cluster0.pq7j4si.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const noteScheme = new mongoose.Schema({
+const noteSchema = new mongoose.Schema({
     content: String,
     important: Boolean,
 })
 
-const = Note = mongoose.model('Note', noteScheme)
+const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
     content: 'HTML is Easy',
