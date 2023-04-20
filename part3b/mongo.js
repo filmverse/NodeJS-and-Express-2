82a05fcf1bc752a2cd2f1e188c5cf5ec05375034
vikas sharma <vkassharma19492@gmail.com>
2023-04-21 01:21:15 +0530

    phonebook database created

--- part3b/mongo.js
+++ part3b/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://vkassharma19492:${password}@cluster0.pq7j4si.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://vkassharma19492:${password}@cluster0.u2locne.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
