4b00c4c2c66c52420fbda40a5b2281a2e6861ff4
vikas sharma <vkassharma19492@gmail.com>
2023-04-21 18:40:03 +0530

    updating module in models/note.js

---
+++ part3a/models/note.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+url = process.env.MONGODB_URL
+
+console.log(`connecting to ${url}`)
+
+mongoose.connect(url).then(
+    console.log('Connected to MONGODB')
+).catch((error) => {
+    console.log(`error connecting to MONGODB: ${error.message}`)
+})
+
+const noteScheme = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+noteScheme.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteScheme)
