aae39baff62a05035a1f57549f6659da7e4b071b
vikas sharma <vkassharma19492@gmail.com>
2023-04-24 03:13:46 +0530

    updating data from express using findByidAndUpdate

--- part3a/index.js
+++ part3a/index.js
@@ -37,6 +37,20 @@ app.get('/api/notes/:id', (request, response, next) => {
     }).catch(error => next(error))
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { 'new': true }).then(
+        updatedNote => response.json(updatedNote)
+    ).catch(
+        error => next(error)
+    )
+})
+
 app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndRemove(request.params.id).then(result => {
         response.status(204).end()
