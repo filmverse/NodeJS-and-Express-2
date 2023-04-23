3c8b1b1cf40584621eb9e6f4f933768fe5f20d28
vikas sharma <vkassharma19492@gmail.com>
2023-04-24 02:14:40 +0530

    bringing an order to middleware

--- part3a/index.js
+++ part3a/index.js
@@ -4,15 +4,18 @@ const app = express()
 const Note = require('./models/note')
 const cors = require('cors')
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({
-        error: "unknown endpoint"
-    })
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
 }
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
@@ -24,17 +27,14 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
         if (note) {
             response.json(note)
         } else {
             response.status(404).end()
         }
-    }).catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id'})
-    })
+    }).catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -59,7 +59,25 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({
+        error: "unknown endpoint"
+    })
+}
+
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
