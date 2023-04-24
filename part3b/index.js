98033ad500cbdb004580e2e443ea1d998baae91c
vikas sharma <vkassharma19492@gmail.com>
2023-04-25 03:20:34 +0530

    updating error message

--- part3b/index.js
+++ part3b/index.js
@@ -58,7 +58,7 @@ app.post('/api/persons', (request, response) => {
     const body = request.body
 
     if (body.name === undefined || body.number === undefined) {
-        return response.status(400).json({error: "content missing"})
+        return response.status(400).json({error: "name or number is missing"})
     }
 
     const person = new Person({
